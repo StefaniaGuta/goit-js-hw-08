b57eb58901e7b288247a759712b7aee9b4cfef40
StefaniaGuta <142820560+StefaniaGuta@users.noreply.github.com>
2024-02-07 11:48:22 -0800

    finish tasks

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle'
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const key = 'videoplayer-current-time';
+const localStorageTime = localStorage.getItem(key);
+
+   
+    player.on('timeupdate', throttle( time =>
+        localStorage.setItem(key, time.seconds), 1000, {
+            leading: false,
+        })
+      );
+
+      if(localStorageTime != null){
+        player.setCurrentTime(+localStorageTime);
+      }
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,33 @@
+import throttle from 'lodash.throttle'
 
+const form = document.querySelector('.feedback-form');
+const key = 'feedback-form-state';
+
+
+
+const saveDataToLocalStorage = () => {
+  const email = form.querySelector('input[name="email"]').value;
+  const message = form.querySelector('textarea[name="message"]').value;
+    localStorage.setItem(key, JSON.stringify({email, message}));
+};
+
+form.addEventListener('input', throttle(saveDataToLocalStorage, 500));
+
+const populateFormFields = () => {
+ const savedState = localStorage.getItem(key);
+  if (savedState) {
+    const { email, message } = JSON.parse(savedState);
+    form.querySelector('input[name="email"]').value = email; 
+    form.querySelector('textarea[name="message"]').value = message; 
+  }
+};
+
+window.addEventListener('load', populateFormFields);
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault(); 
+  const email = form.querySelector('input[name="email"]').value;
+  const message = form.querySelector('textarea[name="message"]').value;
+  console.log(`Email: ${email}`, `Message: ${message}`);
+  localStorage.removeItem(key);
+});
